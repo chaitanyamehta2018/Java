f8f2eadb34d1fe140abbc80e2a2240437ce99cfb
HerryPotter <HerryPotter@DESKTOP-IB501OK>
2024-12-22 21:32:13 +0530

    Added exercise3.java

--- beginners/src/main/java/com/ats/beginners/HelloWorld.java
+++ beginners/src/main/java/com/ats/beginners/HelloWorld.java
@@ -8,7 +8,7 @@ package com.ats.beginners;
  *
  * @author HerryPotter
  */
-public class Beginners {
+public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
