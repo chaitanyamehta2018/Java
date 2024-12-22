2b5e548cf1d342fc7d0186c602854f31b81ada4d
HerryPotter <HerryPotter@DESKTOP-IB501OK>
2024-12-22 21:25:10 +0530

    Added exercise2.java

---
+++ beginners/src/main/java/com/ats/beginners/w3resource/exercise2.java
@@ -0,0 +1,19 @@
+/*
+Write a Java program to print the sum of two numbers.
+Test Data:
+74 + 36
+Expected Output :
+110
+*/
+package com.ats.beginners.w3resource;
+
+
+public class exercise2 
+{
+    public static void main (String args[])
+    {
+        int sum = 74+36;
+        System.out.println(sum);
+    }
+    
+}
