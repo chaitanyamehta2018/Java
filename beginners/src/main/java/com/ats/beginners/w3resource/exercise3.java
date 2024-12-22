a3ebb0cbed392099f9baaaadf55e6989d53b6420
HerryPotter <HerryPotter@DESKTOP-IB501OK>
2024-12-22 21:28:55 +0530

    Added exercise3.java

---
+++ beginners/src/main/java/com/ats/beginners/w3resource/exercise3.java
@@ -0,0 +1,17 @@
+/*
+Write a Java program to divide two numbers and print them on the screen.
+Test Data :
+50/3
+Expected Output :
+16
+ */
+package com.ats.beginners.w3resource;
+
+
+public class exercise3 {
+    public static void main (String args[]){
+        int div = 50/3;
+        System.out.println(div);
+    }
+    
+}
